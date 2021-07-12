2ad1d3f17e454a80b504cf91d0b6b2caa49f17e2
MatejMagat305 <matejmag305@gmail.com>
2021-07-12 11:02:10 +0200

    fix real url

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,5 +1,5 @@
-//export const proxy_url = "http://5.178.48.91:8180";
-export const proxy_url = "http://localhost:7777";
+export const proxy_url = "http://5.178.48.91:8180";
+//export const proxy_url = "http://localhost:7777";
 
 export const TIMEOUT = 60 * 100; // 100 minutes
 
